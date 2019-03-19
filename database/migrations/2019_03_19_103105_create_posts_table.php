defa1230fd390cfeb01ac49e30f91bed06749271
kotoff <alter36@mail.ru>
2019-03-19 15:23:36 +0300

    Posts table migration

--- database/migrations/2019_03_19_103105_create_posts_table.php
+++ database/migrations/2019_03_19_103105_create_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCategoriesTable extends Migration
+class CreatePostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,16 @@ class CreateCategoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('category_id')
+                ->nullable(false)
+                ->comment('Category id');
             $table->string('name', 255)
                 ->nullable(false)
                 ->charset('utf8mb4')
                 ->collation('utf8mb4_unicode_ci')
-                ->comment('Category name');
+                ->comment('Post name');
             $table->timestamps();
         });
     }
@@ -31,6 +34,6 @@ class CreateCategoriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('posts');
     }
 }
